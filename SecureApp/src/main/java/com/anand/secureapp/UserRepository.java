e7a29a108d909e7188bbd3d101c09788518adba0
Vivek Omkar Machade <vivekmachade323@gmail.com>
2025-03-20 11:43:00 +0530

    [Vivek Machade] Add.Spring Boot Security OAuth 2

---
+++ SecureApp/src/main/java/com/anand/secureapp/UserRepository.java
@@ -0,0 +1,8 @@
+package com.anand.secureapp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	User findByUsername(String username);
+
+}
